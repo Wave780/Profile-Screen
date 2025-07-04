8dd4a04a4735ba02d42b91214a3d061ec2883c2a
Wave780 <jonahemma12@gmail.com>
2025-07-04 08:56:04 +0100

    Add Header section

---
+++ lib/skill_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SkillSection extends StatelessWidget {
+  const SkillSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
